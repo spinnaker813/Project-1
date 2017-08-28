c56f6319c1d51f647d235fe4c2dd5d20f3074cbf
spinnaker813 <adam.braaten@outlook.com>
2017-08-28 12:01:39 -0500

    Readme update

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-print('Hello World!');
+epsilon = 0.000000001
+y = 54
+guess = y/2.0
+numGuesses = 0
+
+while abs(guess*guess - y) >= epsilon:
+    numGuesses += 1
+    guess = guess - (((guess**2) - y)/(2*guess))
+print('numGuesses = ' + str(numGuesses))
+print('Square root of ' + str(y) + ' is about ' + str(guess))
