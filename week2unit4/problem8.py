b7abaface30dfd455492ad530ac508523e63e62e
Adam Braaten <adam@Adams-MacBook-Pro.local>
2017-08-31 15:33:18 -0500

    Yup

---
+++ week2unit4/problem8.py
@@ -0,0 +1,13 @@
+"""
+MITx: 6.00.1x Introduction to Computer Science and Programming Using Python
+Write a function called polysum that takes 2 arguments, n and s. 
+This function should sum the area and square of the perimeter of the regular polygon. 
+The function returns the sum, rounded to 4 decimal places.
+
+By: Adam Braaten
+"""
+
+import math
+n = 4
+s = 2
+print(n, s)
